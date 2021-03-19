b0117a2867e1a9c26442f8574992d74f6923f63d
david95j2 <glory20220j@gmail.com>
2021-03-20 00:49:45 +0900

    기존 게시물의 작성자를 회원1로 지정

--- db.sql
+++ db.sql
@@ -57,3 +57,12 @@ loginPw = "user1",
 nickname = "user1",
 cellphoneNo = "01012341234",
 email = "test@gmail.com";
+
+# 게시물 테이블에 회원번호 칼럼 추가
+ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+# 기존 게시물의 작성자를 회원1로 지정
+UPDATE article
+SET memberId = 1
+WHERE memberId = 0
+
