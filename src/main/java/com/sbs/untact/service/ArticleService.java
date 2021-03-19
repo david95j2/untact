b567a7282321daf20d2b7c7a3bdf2632072f7b96
david95j2 <glory20220j@gmail.com>
2021-03-19 17:06:11 +0900

    마이바티스용 게시물 검색로직 적용

--- src/main/java/com/sbs/untact/service/ArticleService.java
+++ src/main/java/com/sbs/untact/service/ArticleService.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sbs.untact.dao.ArticleDao;
 import com.sbs.untact.dto.Article;
