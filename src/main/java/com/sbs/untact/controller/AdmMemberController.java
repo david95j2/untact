1dbd596a31efe689bc2a38b1c1fadb0e79499bf6
david95j2 <glory20220j@gmail.com>
2021-03-23 22:03:19 +0900

    관리자 로그아웃

--- src/main/java/com/sbs/untact/controller/AdmMemberController.java
+++ src/main/java/com/sbs/untact/controller/AdmMemberController.java
@@ -71,4 +71,12 @@ public class AdmMemberController {
 		
 		return memberService.modifyMemberInfoById(param);
 	}
+	
+	@RequestMapping("/adm/member/doLogout")
+	@ResponseBody
+	public String doLogout(HttpSession session) {
+		session.removeAttribute("loginedMemberId");
+
+		return Util.msgAndReplace("로그아웃 되었습니다.", "../member/login");
+	}
 }
