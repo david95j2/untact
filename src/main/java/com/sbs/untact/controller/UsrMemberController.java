deb29902b994b8942917e6cea5aeb271b5134f6d
david95j2 <glory20220j@gmail.com>
2021-03-23 19:42:15 +0900

    로그인 아이디, 로그인 비번으로 authKey 얻는 액션 구현

--- src/main/java/com/sbs/untact/controller/UsrMemberController.java
+++ src/main/java/com/sbs/untact/controller/UsrMemberController.java
@@ -56,7 +56,31 @@ public class UsrMemberController {
 
 		return memberService.join(param);
 	}
+	
+	@RequestMapping("/usr/member/authKey")
+	@ResponseBody
+	public ResultData showAuthKey(String loginId, String loginPw) {
+		if (loginId == null) {
+			return new ResultData("F-1", "아이디를 입력해주세요.");
+		}
 
+		Member memberExisted = memberService.getMemberByLoginId(loginId);
+
+		if (memberExisted == null) {
+			return new ResultData("F-2", "존재하지 않는 아이디입니다.");
+		}
+
+		if (memberExisted.getLoginPw() == null) {
+			return new ResultData("F-1", "비밀번호를 입력해주세요.");
+		}
+
+		if (memberExisted.getLoginPw().equals(loginPw) == false) {
+			return new ResultData("F-3", "비밀빈호가 일치하지 않습니다.");
+		}
+
+		return new ResultData("S-1", String.format("%s님 환영합니다.", memberExisted.getNickname()), "authKey",memberExisted.getAuthKey());
+	}
+	
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
 	public ResultData doLogin(String loginId, String loginPw, HttpSession session) {
@@ -81,7 +105,6 @@ public class UsrMemberController {
 		session.setAttribute("loginedMemberId", memberExisted.getId());
 
 		return new ResultData("S-1", String.format("%s님 환영합니다.", memberExisted.getNickname()));
-
 	}
 
 	@RequestMapping("/usr/member/doLogout")
