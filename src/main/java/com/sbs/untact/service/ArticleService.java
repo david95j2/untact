e27cd28d39e8d5be5cbc3806bdfc0ae93d94393f
david95j2 <glory20220j@gmail.com>
2021-03-25 20:23:14 +0900

    업로드된 파일을 서버의 특정 위치에 올바른 파일명 전략으로 저장

--- src/main/java/com/sbs/untact/service/ArticleService.java
+++ src/main/java/com/sbs/untact/service/ArticleService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.sbs.untact.dao.ArticleDao;
@@ -14,7 +15,7 @@ import com.sbs.untact.util.Util;
 
 @Service
 public class ArticleService {
-
+	
 	@Autowired
 	private ArticleDao articleDao;
 	@Autowired
